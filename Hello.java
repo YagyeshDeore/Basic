17aa1319ce93dddf6e1cd11d24a209f3c494bb6e
Yadnyeshdeore <yadnyesh.imperative@gmail.com>
2025-04-07 22:58:40 +0530

    hello

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+// This is a simple Java program that prints "Hello, World!" to the console.
+// It contains a main method which is the entry point of the program.
+// The program uses the System.out.println() method to output the string to the console.
+// The class is named Hello, and it is public, meaning it can be accessed from other classes.
+// The main method is static, allowing it to be called without creating an instance of the class.
+// The String[] args parameter allows for command-line arguments to be passed to the program.
+// The program can be compiled and run using a Java compiler and runtime environment.
+// To compile the program, use the command: javac Hello.java
+// To run the program, use the command: java Hello
+// The output will be: Hello, World!
+// This program serves as a basic introduction to Java syntax and structure.
+// It demonstrates the use of classes, methods, and the standard output stream.
+// The program can be modified to print different messages or perform other tasks.
