33f98e0b33ad1a481b4a93ea58143e0d9c6527e2
Yadnyeshdeore <yadnyesh.imperative@gmail.com>
2025-04-07 23:20:51 +0530

    Start from basic

---
+++ Python/Hello.py
@@ -0,0 +1,10 @@
+"""
+This script prints a greeting message to the console.
+
+Author: [Yadnyesh]
+Date: [07/04/2025]
+
+Usage:
+    Run the script using a Python interpreter to display the message "Hello".
+"""
+print("Hello")
